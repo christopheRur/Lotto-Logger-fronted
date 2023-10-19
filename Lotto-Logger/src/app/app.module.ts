916c14b23138302851c4150ab7076ff11cdd52e6
Rurangwa <chrisruu@yahoo.com>
2023-10-19 21:31:40 +0800

    updates

--- Lotto-Logger/src/app/app.module.ts
+++ Lotto-Logger/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LottoLoggerComponent } from './components/lotto-logger/lotto-logger.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { LottoLoggerComponent } from './components/lotto-logger/lotto-logger.com
     LottoLoggerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HttpClientModule,FormsModule,
+    RouterModule.forRoot([{path:'lotto-logger',component: LottoLoggerComponent},
+  ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
